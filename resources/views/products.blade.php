dfb91adc1200a7ce5bb6a1c7fb785c2a7ec94e61
Nigel Makwara <nigel@epione.net>
2026-01-07 20:23:30 +0200

    created products blade and added bootstrap link

---
+++ resources/views/products.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Coalition Assessment</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body class="p-4">
+
+
+    </body>
+</html>
