be5a859bed6850653f1f1181161b00cc725186e5
Nigel Makwara <nigel@epione.net>
2026-01-07 20:26:29 +0200

    added product form

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -6,6 +6,29 @@
     </head>
     <body class="p-4">
 
+        <div class="container">
+            <h2 class="mb-4">Product Entry</h2>
 
+            <form id="product-form" class="row g-3">
+                @csrf
+                <div class="col-md-4">
+                    <input type="text" name="product_name" class="form-control" placeholder="Product name" required>
+                </div>
+                <div class="col-md-3">
+                    <input type="number" name="quantity" class="form-control" placeholder="Quantity in stock" required>
+                </div>
+                <div class="col-md-3">
+                    <input type="number" step="0.01" name="price" class="form-control" placeholder="Price per item" required>
+                </div>
+                <div class="col-md-2">
+                    <button class="btn btn-primary w-100">Submit</button>
+                </div>
+            </form>
+
+
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="{{ asset('js/products.js') }}"></script>
     </body>
 </html>
