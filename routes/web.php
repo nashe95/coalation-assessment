7d96170ea64f80edb5dbc43cc887f22711060b1e
Nigel Makwara <nigel@epione.net>
2026-01-07 20:36:00 +0200

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    Route::get('/', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::get('/products/list', [ProductController::class, 'list']);
 });
