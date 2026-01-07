c41319c203518b32b32b46fb6309365671d50f8b
Nigel Makwara <nigel@epione.net>
2026-01-07 20:47:09 +0200

    updated routes to point to products

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('products');
     Route::get('/', [ProductController::class, 'index']);
     Route::post('/products', [ProductController::class, 'store']);
     Route::put('/products/{product}', [ProductController::class, 'update']);
